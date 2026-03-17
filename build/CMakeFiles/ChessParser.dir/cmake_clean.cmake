d31753b9bd8a256adf736a8905450bd3d470e77c
Zack <ichmagkase70@gmail.com>
2026-03-16 22:13:07 -0400

    wrote it in C

---
+++ build/CMakeFiles/ChessParser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessParser.dir/link.d"
+  "CMakeFiles/ChessParser.dir/main.c.o"
+  "CMakeFiles/ChessParser.dir/main.c.o.d"
+  "ChessParser"
+  "ChessParser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ChessParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
